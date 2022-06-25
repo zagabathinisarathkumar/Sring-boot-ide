122521a550c2e5e4c8d1b3b97d9f56a7f586a5a8
zagabathinisarathkumar <105474233+zagabathinisarathkumar@users.noreply.github.com>
2022-06-25 16:57:03 +0530

    Updated coupon

---
+++ coupon-service/src/main/java/com/zensar/repository/CouponRepository.java
@@ -0,0 +1,11 @@
+package com.zensar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.zensar.entity.Coupon;
+
+public interface CouponRepository extends JpaRepository<Coupon, Integer> {
+
+	Coupon findByCouponCode(String couponCode);
+	Coupon deleteByCouponCode(String couponCode);
+}
---
+++ coupon-service/src/main/java/com/zensar/services/CouponService.java
@@ -0,0 +1,16 @@
+package com.zensar.services;
+
+import java.util.List;
+
+import com.zensar.entity.Coupon;
+
+public interface CouponService {
+
+	public Coupon createCoupon(Coupon coupon);
+
+	public Coupon getCoupon(String couponCode);
+	
+	public List<Coupon> getAllCoupons();
+
+	public void  deleteCoupon(String couponCode);
+}
